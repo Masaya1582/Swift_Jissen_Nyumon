8c9938dc4640950b528b3231726b9a2eaeff25e1
Masaya1582 <masa8y88@gmail.com>
2024-10-31 21:40:36 +0900

    Chapter 2

--- Swift_Jissen_Nyumon/Sources/Views/Home/HomeViewController.swift
+++ Swift_Jissen_Nyumon/Sources/Views/Home/HomeViewController.swift
@@ -31,6 +31,16 @@ final class HomeViewController: UIViewController {
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        let number = 123
+        let string = "Hello, World!"
+        type(of: number)
+        type(of: string)
+        print("Stringのカウント: \(string.count)")
+        print("\(string.starts(with: "Hello"))")
+
+        let original = [1, 2, 3]
+        let doubled = original.map( { value in value * 2 } )
+        print("Doubled Values: \(doubled)")
         bind(to: viewModel)
     }
 }
