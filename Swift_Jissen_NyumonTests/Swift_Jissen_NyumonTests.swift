c1bbb4fef1d237b07f81d584ec2b119d2799bcef
Masaya1582 <masa8y88@gmail.com>
2024-10-31 20:57:25 +0900

    Initial Commit

---
+++ Swift_Jissen_NyumonTests/Swift_Jissen_NyumonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Jissen_NyumonTests.swift
+//  Swift_Jissen_Nyumon
+//
+//  Created by MasayaNakakuki on 2024/10/31.
+//
+
+import Testing
+@testable import Swift_Jissen_Nyumon
+
+struct Swift_Jissen_NyumonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
