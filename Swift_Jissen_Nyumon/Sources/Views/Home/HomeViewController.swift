a1de144a3b778529350b9f88e3b379eeddf63eed
Masaya1582 <masa8y88@gmail.com>
2024-10-31 21:57:33 +0900

    Chapter 3

--- Swift_Jissen_Nyumon/Sources/Views/Home/HomeViewController.swift
+++ Swift_Jissen_Nyumon/Sources/Views/Home/HomeViewController.swift
@@ -9,11 +9,17 @@ import RxCocoa
 import RxSwift
 import UIKit
 
+enum Optional<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+
 final class HomeViewController: UIViewController {
     // MARK: - Dependency
     typealias Dependency = HomeViewModelType
 
     // MARK: - Properties
+    private let optionalHello: String? = "Hello"
     private lazy var viewModel: HomeViewModelType = { fatalError("Use (dependency: ) at initialize controller") }()
     private let disposeBag = DisposeBag()
 
@@ -31,7 +37,37 @@ final class HomeViewController: UIViewController {
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        let haiku = """
+        五月雨を
+        あつめて早し
+        最上川
+        """
+        print(haiku)
+
+        let number = 123
+        let numberToString = String(number)
+        print(numberToString)
         bind(to: viewModel)
+
+        let string1 = "Hello"
+        let string2 = "World"
+        print(string1 == string2)
+
+        let how = "How"
+        let are = "are"
+        let you = "you"
+        let howAreYou = how + are + you
+        print(howAreYou)
+
+        let none = Optional<Int>.none
+        print(".note: \(none)")
+
+        let some = Optional<Int>.some(123)
+        print(".some: \(some)")
+
+        if let hello = optionalHello {
+            print(hello)
+        }
     }
 }
 
